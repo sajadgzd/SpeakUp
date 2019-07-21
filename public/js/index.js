2e3450025cc0488a88dd155be95c16ce7674fdcf
sajad1993gh <sajad1993gh@gmail.com>
2019-07-21 19:05:10 -0400

    remove current location finder which was just an extra feauture unused

--- public/js/index.js
+++ public/js/index.js
@@ -23,30 +23,30 @@ function ipLookUp() {
             }
         );
 }
-// ipLookUp();
-if ("geolocation" in navigator) {
-    // check if geolocation is supported/enabled on current browser
-    navigator.geolocation.getCurrentPosition(
-        function success(position) {
-            // for when getting location is a success
-            console.log('Geo latitude', position.coords.latitude,
-                'Geo longitude', position.coords.longitude);
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
+ipLookUp();
+// if ("geolocation" in navigator) {
+//     // check if geolocation is supported/enabled on current browser
+//     navigator.geolocation.getCurrentPosition(
+//         function success(position) {
+//             // for when getting location is a success
+//             console.log('Geo latitude', position.coords.latitude,
+//                 'Geo longitude', position.coords.longitude);
+//             latitude = position.coords.latitude;
+//             longitude = position.coords.longitude;
 
-        },
-        function error(error_message) {
-            // for when getting location results in an error
-            console.error('An error has occured while retrieving' +
-                'location', error_message)
-            ipLookUp()
-        });
-} else {
-    // geolocation is not supported
-    // get your location some other way
-    console.log('geolocation is not enabled on this browser')
-    ipLookUp()
-}
+//         },
+//         function error(error_message) {
+//             // for when getting location results in an error
+//             console.error('An error has occured while retrieving' +
+//                 'location', error_message)
+//             ipLookUp()
+//         });
+// } else {
+//     // geolocation is not supported
+//     // get your location some other way
+//     console.log('geolocation is not enabled on this browser')
+//     ipLookUp()
+// }
 
 
 // global function that will be used to post the most recent crimes whenever the page loads
