d3c14663b3dabac4507e05639e56628e2ddecea0
sajad1993gh <sajad1993gh@gmail.com>
2019-07-21 00:46:07 -0400

    added required code for user registration

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,10 @@ var db = require("../models");
 var moment = require("moment");
 var geocoder = require('google-geocoder');
 // console.log(moment());
+
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
 var Sequelize = require("sequelize");
 module.exports = function(app) {
 
@@ -84,7 +88,7 @@ module.exports = function(app) {
         });
 
 
-    // Create a new CrimeCrime
+    // Create a new Crime
     app.post("/api/new/Crime", function(req, res) {
 
 
@@ -120,6 +124,98 @@ module.exports = function(app) {
 
 
     });
+
+
+
+    /* Register Route
+    ========================================================= */
+    app.post('/register', async(req, res) => {
+
+        // hash the password provided by the user with bcrypt so that
+        // we are never storing plain text passwords. This is crucial
+        // for keeping your db clean of sensitive data
+        const hash = bcrypt.hashSync(req.body.password, 10);
+
+        try {
+            // create a new user with the password hash from bcrypt
+            let user = await User.create(
+                Object.assign(req.body, { password: hash })
+            );
+
+            // data will be an object with the user and it's authToken
+            let data = await user.authorize();
+
+            // send back the new user and auth token to the
+            // client { user, authToken }
+            return res.json(data);
+
+        } catch (err) {
+            return res.status(400).send(err);
+        }
+
+    });
+
+    /* Login Route
+    ========================================================= */
+    app.post('/login', async(req, res) => {
+        const { username, password } = req.body;
+
+        // if the username / password is missing, we use status code 400
+        // indicating a bad request was made and send back a message
+        if (!username || !password) {
+            return res.status(400).send(
+                'Request missing username or password param'
+            );
+        }
+
+        try {
+
+            // we will cover the user authenticate method in the next section
+            let user = await User.authenticate(username, password)
+
+            return res.json(user);
+
+        } catch (err) {
+            return res.status(400).send('invalid username or password');
+        }
+
+    });
+
+    /* Logout Route
+    ========================================================= */
+    app.delete('/logout', async(req, res) => {
+
+        // because the logout request needs to be send with
+        // authorization we should have access to the user
+        // on the req object, so we will try to find it and
+        // call the model method logout
+        const { user, cookies: { auth_token: authToken } } = req
+
+        // we only want to attempt a logout if the user is
+        // present in the req object, meaning it already
+        // passed the authentication middleware. There is no reason
+        // the authToken should be missing at this point, check anyway
+        if (user && authToken) {
+            await req.user.logout(authToken);
+            return res.status(204).send()
+        }
+
+        // if the user missing, the user is not logged in, hence we
+        // use status code 400 indicating a bad request was made
+        // and send back a message
+        return res.status(400).send({ errors: [{ message: 'not authenticated' }] });
+    });
+
+    /* Me Route - get the currently logged in user
+    ========================================================= */
+    app.get('/me', (req, res) => {
+        if (req.user) {
+            return res.send(req.user);
+        }
+        res.status(404).send({ errors: [{ message: 'missing auth token' }] });
+    });
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const customAuthMiddleware = require('./middleware/custom-auth-middleware');
+
 var db = require("./models");
 
 var app = express();
@@ -12,12 +17,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// use the cookie-parser to help with auth token,
+// it must come before the customAuthMiddleware
+app.use(cookieParser());
+app.use(customAuthMiddleware);
+
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
@@ -30,18 +40,18 @@ var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    app.listen(PORT, function() {
+        console.log(
+            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            PORT,
+            PORT
+        );
+    });
 });
 
 module.exports = app;
